8dfb74a26e91217ecdf6e591b5c0794ab97ec6a1
nikafilishvili <filishvilinika2@gmail.com>
2024-03-26 16:40:40 +0400

    second commit

--- day7/day7.py
+++ day7/day7.py
@@ -20,3 +20,15 @@ print(area)
 
 num = int(input("please enter number: "))
 print(num > 100 and num % 9 == 0)
+
+#5
+
+print(100 > 110 and 30 < 3)
+print(60 < 30 and 100 > 30)
+print(200 > 135 and 100 < 67)
+print(155 > 35 and 65 < 134)
+
+print(176 > 200 or 32 < 12)
+print(200 < 34 or 100 > 66)
+print(189 > 79 or 200 < 55)
+print(199 > 36 or 300 < 679)
--- goa codes classwork/day7classwork.py
+++ goa codes classwork/day7classwork.py
@@ -9,3 +9,7 @@ print(number > 10 and number %2 == 0)
 
 age = int(input("please enter your age; "))
 print(age > 0 and age < 30)
+
+num1 = int(input("please enter number: "))
+num2 = num1 + 20
+print(num1 < num2)
