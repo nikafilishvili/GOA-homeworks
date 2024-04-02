21ff86530003278944e0bec3e2e144a8fda76cf0
nikafilishvili <filishvilinika2@gmail.com>
2024-04-02 14:28:02 +0400

    second commit

---
+++ day9/day9.py
@@ -0,0 +1,9 @@
+
+salary = int(input("please enter your salary: "))
+
+if salary > 10000:
+    print("goashi swavlobdi?")
+elif salary >= 1000 or salary == 10000:
+    print("you mid")
+else:
+    print("shemosuliyavi goashi")
---
+++ goa codes classwork/day9classwork.py
@@ -0,0 +1,5 @@
+num1 = input("please enter number: ")
+num2 = input("please enter number: ")
+
+if num1 > num2:
+    print(goa magaria)
