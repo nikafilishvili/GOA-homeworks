72d910a8026c31f975970a9d8fdd7a414d010a8c
nikafilishvili <filishvilinika2@gmail.com>
2024-04-16 17:08:11 +0400

    second commit

--- day10/day10.py
+++ day10/day10.py
@@ -1,7 +1,7 @@
 
 #1
 
-academy = input("please neter which programming academy are you learning: ")
+academy = input("please enter which programming academy are you learning: ")
 
 if academy == "GOA":
     print("correct")
@@ -26,3 +26,28 @@ if user_num >= 5:
     print(user_num * 2)
 else:
     print(user_num * 4)
+
+#4
+
+ticket_price = 10
+ticket_num = int(input("please enter how many tickets do you want: "))
+
+if ticket_num < 5:
+    print(ticket_num * 10)
+else:
+    print(ticket_num * 7)
+
+#5
+
+num1 = int(input("please enter number: "))
+num2 = int(input("please neter number: "))
+operation = input("please enter which operations do you want to do from this four  +, -, *, /. : ")
+                
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num2)
--- goa codes classwork/day9classwork.py
+++ goa codes classwork/day9classwork.py
@@ -1,5 +1,19 @@
-num1 = input("please enter number: ")
-num2 = input("please enter number: ")
+
+num1 = int(input("please enter number: "))
+num2 = int(input("please enter number: "))
 
 if num1 > num2:
-    print(goa magaria)
+    print("goa magaria")
+
+
+
+mom = "ia"
+dad = "levani"
+sister = "anano"
+brother = "gio"
+
+if mom == "ia" and dad == "levani":
+    print("true")
+
+if sister == "anano" and brother == "gio":
+    print("true")
