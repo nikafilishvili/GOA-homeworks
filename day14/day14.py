4fea200a187164f34b7a45a86b819571f8686659
nikafilishvili <filishvilinika2@gmail.com>
2024-05-11 12:17:23 +0400

    second commit

---
+++ day14/day14.py
@@ -0,0 +1,37 @@
+for i in range(2, 30, 3):
+    print(i)
+
+
+for i in range(2, 6):
+    print(i)
+
+
+for i in range(3, 60, 4):
+    print(i)
+
+
+for i in range(0, 100, 5):
+    print(i)
+
+
+for i in range(0, 90, 3):
+    print(i)
+
+
+i = 10
+while i < 20:
+    print(i)
+    i += 1
+
+
+i = 0
+while i < 8:
+  i += 1
+  if i == 4:
+    print(i)
+
+
+i = 3
+while i < 19:
+    print(i)
+    i += 2
